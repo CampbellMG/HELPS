9cc185a53eb0b12f88364b5e935bf258e7dcaf7d
Diego <diegofportela1@gmail.com>
2019-04-19 21:52:06 +1000

    changed route, added API Methods

--- HELPS/Controllers/SWorkshopController.cs
+++ HELPS/Controllers/SWorkshopController.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace HELPS.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/student/workshops")]
     public class SWorkshopController : Controller
     {
         private readonly HelpsContext _context;
@@ -17,6 +17,32 @@ namespace HELPS.Controllers
             this._context = _context;
         }
 
+        //api/student/workshops/id
+        [HttpGet]
+        public String Get(int id)
+        {
+            return "function get";
+        }
 
+        //api/student/workshops/id
+        [HttpPost]
+        public String post(int id)
+        {
+            return "function post";
+        }
+
+        //api/student/workshops/id
+        [HttpPut]
+        public void Put(int id)
+        {
+            
+        }
+
+        //api/student/workshops/id
+        [HttpDelete]
+        public void delete()
+        {
+            
+        }
     }
 }
