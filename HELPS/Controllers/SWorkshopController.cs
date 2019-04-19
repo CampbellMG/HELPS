b15572889036a726670bcae7ed0debdc71e7423c
Diego <diegofportela1@gmail.com>
2019-04-19 21:22:11 +1000

    Added API Controller scaffold

---
+++ HELPS/Controllers/SWorkshopController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HELPS.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HELPS.Controllers
+{
+    [Route("api/[controller]")]
+    public class SWorkshopController : Controller
+    {
+        private readonly HelpsContext _context;
+
+        public SWorkshopController(HelpsContext _context)
+        {
+            this._context = _context;
+        }
+
+
+    }
+}
