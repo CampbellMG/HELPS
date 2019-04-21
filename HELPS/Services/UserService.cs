268ed265668b77bf15c95f3be8e151579df6ad88
Diego <diegofportela1@gmail.com>
2019-04-22 01:53:40 +1000

    basis of JWT_Authentication, needs to implement a database and route correctly to the specification for front end

--- HELPS/Services/UserService.cs
+++ HELPS/Services/UserService.cs
@@ -19,7 +19,7 @@ namespace HELPS.Services
 
     public class UserService : IUserService
     {
-        // users hardcoded for simplicity, store in a db with hashed passwords in production applications
+        // users hardcoded for simplicity, store in a db with hashed passwords
         private List<User> _users = new List<User>
         {
             new User { Id = 1, FirstName = "Test", LastName = "User", Username = "test", Password = "test" }
