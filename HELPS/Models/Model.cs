58b6a45dec898de5f3b594c500dc1998634311ec
Roshana Khan <roshana.khan@live.com>
2019-05-16 17:07:42 +1000

    Added Messages part

--- HELPS/Models/Model.cs
+++ HELPS/Models/Model.cs
@@ -10,11 +10,19 @@ namespace HELPS.Models
         { }
 
         public DbSet<Student> Students { get; set; }
+        public DbSet<Message> Messages { get; set; }
     }
 
     public class Student
     {
         public int StudentId { get; set; }
         public string Name { get; set; }
+    }
+
+    public class Message
+    {
+        public int MessageId { get; set; }
+        public string MessageTitle { get; set; }
+        public string MessageContent { get; set; }
     }
 }
