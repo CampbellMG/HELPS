cf4d83b4494e3186e7a44af156e41a6a46af89e5
Matthew Delotavo <matthew.t.delotavo@gmail.com>
2019-05-26 16:51:24 +1000

    Add 'reports' endpoint

---
+++ HELPS/Controllers/ReportController.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HELPS.Models;
+using Microsoft.AspNetCore.Mvc;
+using AppContext = System.AppContext;
+
+namespace HELPS.Controllers
+{
+    [Route("api/[controller]")]
+    public class ReportController : Controller
+    {
+        private readonly HelpsContext _context;
+
+        public ReportController(HelpsContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Report>>> GetReports()
+        {
+            return await _context.Reports.ToListAsync();
+        }*/
+
+        /*[HttpGet({"id"})]
+        public async Task<ActionResult<Report>> GetReport(int id)
+        {
+            var reportItem = await _context.Reports.FindAsync(id);
+
+            if (reportItem == null)
+            {
+                return NotFound();
+            }
+
+            return reportItem;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Report>> PostReport(Report report)
+        {
+            _context.Reports.Add(report);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameOf(GetReport), new { id = report.Id }, report);
+        }
+
+    }
+}
--- HELPS/Models/Model.cs
+++ HELPS/Models/Model.cs
@@ -10,11 +10,23 @@ namespace HELPS.Models
         { }
 
         public DbSet<Student> Students { get; set; }
+        public DbSet<Report> Reports { get; set; }
     }
 
     public class Student
     {
         public int StudentId { get; set; }
         public string Name { get; set; }
+    }
+
+    public class Report
+    {
+        public string From { get; set; }
+        public string To { get; set; }
+        public int Id { get; set; }
+        public string SkillSet { get; set; }
+        public string Topic { get; set; }
+        public int SessionId { get; set; }
+        public bool Generate { get; set; }
     }
 }
