550acb23cfb849b20d6ae4021244299daf20b91a
Venkata B <venkatab144@gmail.com>
2019-05-28 20:45:00 +1000

    added all required student properties

---
+++ HELPS/Migrations/20190528104231_StudentPropertiesComplete.cs
@@ -0,0 +1,177 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HELPS.Migrations
+{
+    public partial class StudentPropertiesComplete : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CountryOfOrigin",
+                table: "Students",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Course",
+                table: "Students",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "DOB",
+                table: "Students",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Degree",
+                table: "Students",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "EducationalBackground",
+                table: "Students",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "Students",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Faculty",
+                table: "Students",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "FirstLanguage",
+                table: "Students",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Gender",
+                table: "Students",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "HasCompletedCAF",
+                table: "Students",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "HomePhone",
+                table: "Students",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsSpecialNeeds",
+                table: "Students",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "MobileNumber",
+                table: "Students",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Other",
+                table: "Students",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "PrefFirstName",
+                table: "Students",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "RegisteredDate",
+                table: "Students",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Status",
+                table: "Students",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Year",
+                table: "Students",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CountryOfOrigin",
+                table: "Students");
+
+            migrationBuilder.DropColumn(
+                name: "Course",
+                table: "Students");
+
+            migrationBuilder.DropColumn(
+                name: "DOB",
+                table: "Students");
+
+            migrationBuilder.DropColumn(
+                name: "Degree",
+                table: "Students");
+
+            migrationBuilder.DropColumn(
+                name: "EducationalBackground",
+                table: "Students");
+
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "Students");
+
+            migrationBuilder.DropColumn(
+                name: "Faculty",
+                table: "Students");
+
+            migrationBuilder.DropColumn(
+                name: "FirstLanguage",
+                table: "Students");
+
+            migrationBuilder.DropColumn(
+                name: "Gender",
+                table: "Students");
+
+            migrationBuilder.DropColumn(
+                name: "HasCompletedCAF",
+                table: "Students");
+
+            migrationBuilder.DropColumn(
+                name: "HomePhone",
+                table: "Students");
+
+            migrationBuilder.DropColumn(
+                name: "IsSpecialNeeds",
+                table: "Students");
+
+            migrationBuilder.DropColumn(
+                name: "MobileNumber",
+                table: "Students");
+
+            migrationBuilder.DropColumn(
+                name: "Other",
+                table: "Students");
+
+            migrationBuilder.DropColumn(
+                name: "PrefFirstName",
+                table: "Students");
+
+            migrationBuilder.DropColumn(
+                name: "RegisteredDate",
+                table: "Students");
+
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "Students");
+
+            migrationBuilder.DropColumn(
+                name: "Year",
+                table: "Students");
+        }
+    }
+}
--- HELPS/Models/Model.cs
+++ HELPS/Models/Model.cs
@@ -16,6 +16,25 @@ namespace HELPS.Models
     {
         public int StudentId { get; set; }
         public string Name { get; set; }
+		public string RegisteredDate { get; set; }
+		public string PrefFirstName { get; set; }
+		public string Faculty { get; set; }
+		public string Course { get; set; }
+		public string Email { get; set; }
+		public string HomePhone { get; set; }
+		public string MobileNumber { get; set; }
 		public string BestContactNumber { get; set; }
+		public string DOB { get; set; }
+		public string Gender { get; set; }
+		public string Degree { get; set; }
+		public string Year { get; set; }
+		public string Status { get; set; }
+		public string FirstLanguage { get; set; }
+		public string CountryOfOrigin { get; set; }
+		public string EducationalBackground { get; set; }
+		public bool HasCompletedCAF { get; set; }
+		public bool IsSpecialNeeds { get; set; }
+		public string Other { get; set; }
+
 	}
 }
