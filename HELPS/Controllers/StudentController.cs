a444bb2c7e0f8c9126bd1af6b2b49ac2d63cd172
Venkata B <venkatab144@gmail.com>
2019-05-28 21:15:02 +1000

    added Student GET and PUT endpoints

--- HELPS/Controllers/StudentController.cs
+++ HELPS/Controllers/StudentController.cs
@@ -4,19 +4,47 @@ using System.Linq;
 using System.Threading.Tasks;
 using HELPS.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using AppContext = System.AppContext;
 
 namespace HELPS.Controllers
 {
-    [Route("api/[controller]")]
-    public class StudentController : Controller
-    {
-        private readonly HelpsContext _context;
+	[Route("api/[controller]")]
+	public class StudentController : Controller
+	{
+		private readonly HelpsContext _context;
 
-        public StudentController(HelpsContext context)
-        {
-            _context = context;
-        }
+		public StudentController(HelpsContext context)
+		{
+			_context = context;
+		}
+
+		[HttpGet("{id}")]
+		public async Task<ActionResult<Student>> GetStudent(int id)
+		{
+			var student = await _context.Students.FindAsync(id);
+			
+			if (student == null)
+			{
+				return NotFound();
+			}
+
+			return student;
+		}
+
+		[HttpPut("{id}")]
+		public async Task<IActionResult> PutStudent(int id, [FromBody] Student student)
+		{
+			if (id != student.StudentId)
+			{
+				return NotFound();
+			}
+
+			_context.Entry(student).State = EntityState.Modified;
+			await _context.SaveChangesAsync();
+
+			return NoContent();
+		}
 
 		/// <summary>
 		/// Returns students in a JSON format
@@ -24,9 +52,9 @@ namespace HELPS.Controllers
 		/// </summary>
 		/// <returns>JSON formatted list of students</returns>
 		[HttpGet]
-        public List<Student> GetAllStudents()
+        public async Task<ActionResult<IEnumerable<Student>>> GetAllStudents()
         {
-            return _context.Students.ToList();
+            return await _context.Students.ToListAsync();
         }
         
     }
