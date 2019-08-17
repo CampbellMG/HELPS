9444f5735fcc2fa6b3867a296546bfc23445f343
Matthew Delotavo <matthew.t.delotavo@student.uts.edu.au>
2019-08-17 23:00:19 +1000

    Update Migrations

---
+++ HELPS/Migrations/20190817123730_Workshop.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HELPS.Migrations
+{
+    public partial class Workshop : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "SkillSet",
+                table: "Workshops",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "RoomId",
+                table: "Reports",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SkillSet",
+                table: "Workshops");
+
+            migrationBuilder.DropColumn(
+                name: "RoomId",
+                table: "Reports");
+        }
+    }
+}
