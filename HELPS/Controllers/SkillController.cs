bba935298c1e2ace7d83be97876a0bd753d28935
diegoportela99 <13286378@student.uts.edu.au>
2019-08-19 16:55:52 +1000

    Skills API

---
+++ HELPS/Controllers/SkillController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using HELPS.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace HELPS.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    //The Message class
+    public class SkillController : StudentUserController
+    {
+        public SkillController(HelpsContext context) : base(context)
+        {
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Skill>>> GetSkills()
+        {
+            if (!IsAdmin()) return Unauthorized();
+
+            return await Context.Skills.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Skill>> GetSkill(int id)
+        {
+            if (!IsAdmin()) return Unauthorized();
+
+            var skill = await Context.Skills.FindAsync(id);
+
+            if (skill == null) return NotFound();
+
+            return skill;
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutSkill(int id, [FromBody] Skill skills)
+        {
+            if (!IsAdmin()) return Unauthorized();
+
+            if (id != skills.Id) return BadRequest();
+
+            Context.Entry(skills).State = EntityState.Modified;
+            await Context.SaveChangesAsync();
+
+            return NoContent();
+        }
+    }
+}
---
+++ HELPS/Migrations/20190818162141_AddSkills.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HELPS.Migrations
+{
+    public partial class AddSkills : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
