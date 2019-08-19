63ce977f00d67a1db4a0534df92714997e229283
Diego Portela <37290513+diegoportela99@users.noreply.github.com>
2019-08-19 17:12:13 +1000

    Update 20190818162141_AddSkills.cs

--- HELPS/Migrations/20190818162141_AddSkills.cs
+++ HELPS/Migrations/20190818162141_AddSkills.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 namespace HELPS.Migrations
 {
@@ -6,12 +7,24 @@ namespace HELPS.Migrations
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-
+            migrationBuilder.CreateTable(
+                name: "Skills",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn),
+                    Title = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Skills", x => x.Id);
+                });
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-
+            migrationBuilder.DropTable(
+                name: "Skills");
         }
     }
 }
